52e5ce940800a3c88b8e5c0ca1bb6d4066a8f209
Shadow <48737091+SirPickledLemon@users.noreply.github.com>
2020-10-13 22:33:04 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,35 @@
 print("Hello World")
+#convert elevator floors
+inp = input('Europe floor: ')
+usf = int(inp) + 1
+print('US floor' , usf)
+
+#if statements
+x = 5
+if x < 10:
+    print('Smaller')
+if x > 20:
+    print('Bigger')
+print('Finish')
+
+#try/except to move past a traceback error (if this blows up, it moves onto different code rather than stopping)
+astr = 'Hello Bob'
+try:
+    istr = int(astr)
+except:
+    istr = -1
+print('First' , istr)
+astr = '123'
+try:
+    istr = int(astr)
+except:
+    istr = -1
+print('Second' , istr)
+
+#define function (def)
+def thing():
+    print('Hello')
+    print('Fun')
+thing()
+print('Wow')
+thing()
